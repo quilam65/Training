ae226d6cae6f7095c628ac4f9ea8ef3bd5cda7bc
anhqui1995 <quilam.ct@gmail.com>
2018-03-15 16:16:01 +0700

    fix rollback migration

--- db/migrate/20180315091000_change_products_price.rb
+++ db/migrate/20180315091000_change_products_price.rb
@@ -1,5 +1,9 @@
 class ChangeProductsPrice < ActiveRecord::Migration[5.1]
-  def change
+  def up
     change_column :products, :price, :decimal, precision: 10, scale: 2
+  end
+
+  def down
+    change_column :products, :price, :decimal, precision: 6, scale: 2
   end
 end
