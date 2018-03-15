ec839131e7be8a2ed42ca2bb68ad22d2ef637655
anhqui1995 <quilam.ct@gmail.com>
2018-03-15 15:58:47 +0700

    add belong to category to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315084555) do
+ActiveRecord::Schema.define(version: 20180315085650) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180315084555) do
     t.text "description"
     t.decimal "price", precision: 6, scale: 2
     t.boolean "published"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
