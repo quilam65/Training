1135419ead8f5889d0a6dda16486911bf2dafa6b
anhqui1995 <quilam.ct@gmail.com>
2018-03-15 15:38:14 +0700

    remove short_description

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315082834) do
+ActiveRecord::Schema.define(version: 20180315083619) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.decimal "price", precision: 6, scale: 2
     t.boolean "published"
-    t.string "short_description"
   end
 
 end
