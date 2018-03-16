ca99368f605b455a99a4bb5bfec685bea60cf075
anhqui1995 <quilam.ct@gmail.com>
2018-03-16 10:42:20 +0700

    add scope for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validate :title_is_shorter_than_description
 
+  scope :published, ->{where(published: true)}
+  scope :priced_more_then, ->(price) {where('price >?', price)}
+
   def title_is_shorter_than_description
     return if title.blank? or description.blank?
     if description.length < title.length
