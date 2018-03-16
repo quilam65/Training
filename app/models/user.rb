f46ddff02459002eb6a806b5bb30cba0cf5c03b1
anhqui1995 <quilam.ct@gmail.com>
2018-03-16 09:19:10 +0700

    delete function validates in application

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
- # email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
- # validates :email, :presence => true,
- #                  :format    => {  :with => email_regex  }
  # validation_email
  # validation_domain
  validates :email, email: true
