07260215f17234d2ff747264573571efc1118b28
anhqui1995 <quilam.ct@gmail.com>
2018-03-19 17:20:29 +0700

    add vendor

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module AppTrain
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.autoload_paths += %W["#{config.root}/app/validators/"]
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
